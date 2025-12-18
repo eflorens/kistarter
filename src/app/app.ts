6a82e0bb58149259c151674fa92085f37202c0a1
eflorens <florian.escude@gmail.com>
2025-12-18 15:45:50 +0100

    adding angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule],
   template: `
     <h1>Welcome to {{ title() }}!</h1>
-
+    <mat-slide-toggle></mat-slide-toggle>
     <router-outlet />
   `,
   styles: [],
